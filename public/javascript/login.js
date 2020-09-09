5cf5d394e5c4ebbfdf390d62b55059418f506a53
Alex Donnelly <a.james.donnelly@gmail.com>
2020-09-08 21:01:05 -0500

    typo updates

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -26,23 +26,23 @@ async function signupFormHandler(event) {
     event.preventDefault();
 
     const username = document.querySelector('#username-signup').value.trim();
-    const address = document.querySelector('#street-address').value.trim();
+    const street_address = document.querySelector('#street-address').value.trim();
     const city = document.querySelector('#city').value.trim();
     const zipcode = document.querySelector('#zipcode').value.trim();
-    const residence = document.querySelector('#years-at-residence').value.trim();
+    const years_at_address = document.querySelector('#years-at-address').value.trim();
     const famSize = document.querySelector('#family-size').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
     
-    if (username && address && city && zipcode && residence && famsize && email && password) {
-        const response = await fetch ('/api/member-routes.js', {
+    if (username && street_address && city && zipcode && years_at_address && famSize && email && password) {
+        const response = await fetch ('/api/members', {
             method: 'post',
             body: JSON.stringify({
                 username,
-                address,
+                street_address,
                 city,
                 zipcode,
-                residence,
+                years_at_address,
                 famSize,
                 email,
                 password
