fa7f17818a4a880e636f63aa1753794b83b21db5
Alex Donnelly <a.james.donnelly@gmail.com>
2020-09-08 20:18:58 -0500

    updates to login code

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -26,14 +26,24 @@ async function signupFormHandler(event) {
     event.preventDefault();
 
     const username = document.querySelector('#username-signup').value.trim();
+    const address = document.querySelector('#street-address').value.trim();
+    const city = document.querySelector('#city').value.trim();
+    const zipcode = document.querySelector('#zipcode').value.trim();
+    const residence = document.querySelector('#years-at-residence').value.trim();
+    const famSize = document.querySelector('#family-size').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-
-    if (username && email && password) {
-        const response = await fetch ('/api/members', {
+    
+    if (username && address && city && zipcode && residence && famSize && email && password) {
+        const response = await fetch ('/api/member-routes.js', {
             method: 'post',
             body: JSON.stringify({
                 username,
+                address,
+                city,
+                zipcode,
+                residence,
+                famSize,
                 email,
                 password
             }),
@@ -41,7 +51,7 @@ async function signupFormHandler(event) {
         });
 
         if (response.ok) {
-            document.location.replace('/yourpage/');
+            document.location.replace('/views/yourpage/');
         } else {
             alert(response.statusText);
         }
