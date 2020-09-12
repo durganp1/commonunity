ef0fb88db24cc715091e933fb66c3eb75d96d952
BradD <bradleyeday@gmail.com>
2020-09-12 13:53:04 -0500

    resolved conflicting changes

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ const sess = {
   
   app.use(routes);
   
-  sequelize.sync({ force: false }).then(() => {
+  sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
   });
