b1c89f48346de88ce71cbcd920f7aa369c9c4f73
Taryn Fanning <taryn.fanning@gmail.com>
2020-09-13 21:24:26 -0500

    edit function works but only pulls first post

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,8 +1,38 @@
+// async function deleteFormHandler(event) {
+//     event.preventDefault();
+//     await fetch(`/api/posts/${post.id}`, {
+//         method: 'DELETE'
+//       });
+//   }
+  
+//   document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
+
 async function deleteFormHandler(event) {
     event.preventDefault();
-    await fetch(`/api/posts/${post.id}`, {
-        method: 'DELETE'
-      });
+  
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE'
+    });
+
+    // const response2 = await fetch(`/api/comments/`, {
+    //     method: 'DELETE'
+    // });
+  
+    if (response.ok) {
+      document.location.replace('/yourpage/');
+    } else {
+      alert(response.statusText);
+    }
+
+    // if (response2.ok) {
+    //     document.location.replace('/yourpage/');
+    // } else {
+    //     alert(response.statusText);
+    // }
   }
   
   document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
+  
--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,15 +1,53 @@
-async function editFormHandler(event) {
-    event.preventDefault();
-    await fetch(`/api/posts/${post.id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-          title,
-          post_message
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-  }
+// async function editFormHandler(event) {
+
+//     event.preventDefault();
+
+//     const title = document.querySelector('input[name="post-title"]').value;
+//     const post_message = document.querySelector('input[name="post-text"]').value;
+
+//    const response = await fetch(`/api/posts/`, {
+//         method: 'POST',
+//         body: JSON.stringify({
+//           title,
+//           post_message
+//         }),
+//         headers: { 'Content-Type': 'application/json' }
+//       });
+//       console.log(response);
+
+//       if (response.ok) {
+//         document.location.reload();
+//       } else {
+//         alert(response.statusText);
+//       }
+//   }
   
-  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+//   document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+
+async function editFormHandler(event) {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value.trim();
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+  const post_message = document.querySelector('input[name="post-message"]').value;
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      title,
+      post_message
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    document.location.replace('/yourpage');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
